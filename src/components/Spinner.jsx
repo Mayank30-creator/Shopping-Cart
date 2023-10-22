4156487eb43342d203803636d664e239f9b01480
Mayank30-creator <agrawalmayank3001@gmail.com>
2023-10-22 23:56:26 +0530

    First Commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Spinner.css"
+const Spinner = () => {
+  return (
+    <div className='h-full w-full flex justify-center items-center'>
+      <div className="spinner mt-[15%]"></div>
+    </div>
+  );
+}
+
+export default Spinner
